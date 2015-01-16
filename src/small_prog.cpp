3f5a50a01844429fc2a32e1c5e621fd1cdb994a3
Martin <toldamtorp@gmail.com>
2015-01-16 06:43:55 -0500

    set up test framework

---
+++ src/small_prog.cpp
@@ -0,0 +1,9 @@
+#include "small_prog.h"
+
+int f() {
+	return 1;
+}
+
+int g() {
+	return 2;
+}
---
+++ test_file.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+#include "src/small_prog.h"
+
+
+TEST(Functionf Firsttest) {
+	EXPECT_EQ(2, f());
+}
