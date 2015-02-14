8f6259e60b0b901fd7e61276efc680c5fb71547f
Martin <toldamtorp@gmail.com>
2015-02-14 05:31:48 -0500

    Removed garbage

--- Default/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := /home/torp/dev/lib/gtest-1.7.0/build/libgtest.a /home/torp/dev/lib/gtest-1.7.0/build/libgtest_main.a
-
-LIBS := -lpthread
-
--- Default/src/brick.d
+++
@@ -1,5 +0,0 @@
-src/brick.d: ../src/brick.cpp ../src/brick.h ../src/color.h
-
-../src/brick.h:
-
-../src/color.h:
